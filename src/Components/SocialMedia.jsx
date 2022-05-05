4b18b614ceeba17c635941c33c22f616f586a180
enzo filippo <enzo.filippo23@gmail.com>
2022-05-05 19:05:15 +0200

    sm rel fix

--- src/Components/SocialMedia.jsx
+++ src/Components/SocialMedia.jsx
@@ -8,7 +8,7 @@ const SocialMedia = () => {
         whileHover={{ scale: 1.1 }}
         transition={{ duration: 0.5, type: "tween" }}
       >
-        <a href="https://github.com/enzof23" target="_blank">
+        <a href="https://github.com/enzof23" target="_blank" rel="noreferrer">
           <BsGithub />
         </a>
       </motion.div>
@@ -19,6 +19,7 @@ const SocialMedia = () => {
         <a
           href="https://www.linkedin.com/in/enzo-filippo-31559bab/"
           target="_blank"
+          rel="noreferrer"
         >
           <BsLinkedin />
         </a>
